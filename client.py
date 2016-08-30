0d21ff489b7c6d89c8e41150ff7133e07b6c05f0
rajebi <rajebi@yahoo.com>
2016-08-30 17:26:27 +0530

    added comments and cleaned up

--- client.py
+++ client.py
@@ -5,10 +5,16 @@ print ("hello world")
 #                Sockets
 #
 #     Socket client example in python
+#
+# This script creates client thread and socket
 #=============================================
  
 import socket   #for sockets
 import sys  	#for exit
+
+
+host = 'localhost'	# Symbolic name, meaning all available interfaces
+port = 8888 		# Arbitrary non-privileged port
 
 #-------------------------------------
 #create an AF_INET, STREAM socket (TCP)
@@ -21,13 +27,10 @@ except socket.error:
  
 print (" Client Socket is Created")
 #-------------------------------------------------------------
-# Now get IP address of remote host and connect to its server 
+# get IP address of remote host and connect to its server 
 #-------------------------------------------------------------
-host = 'localhost'
-port = 8888
- 
 try:
-    remote_ip = socket.gethostbyname( host )
+    remote_ip = socket.gethostbyname( host )  # note, in this example we are using local host
  
 except socket.gaierror:
     #could not resolve
@@ -44,6 +47,9 @@ while 1:
 
 	# input message
 	message = input("Enter your name: ") 
+	if (message == 'end'):
+		print("end sent, closing connection")
+		break
     # Send the whole string
 	# The first argument (message) needs to be bytes, but you're passing a string. 
 	# You should encode it before sending e.g. message.encode('utf-8'). If you 
--- server.py
+++ server.py
@@ -12,12 +12,12 @@ import sys  	#for exit
 import _thread
 
 
-host = 'localhost'# Symbolic name, meaning all available interfaces
-port = 8888 # Arbitrary non-privileged port
+host = 'localhost'	# Symbolic name, meaning all available interfaces
+port = 8888 		# Arbitrary non-privileged port
 
-#-------------------------------------
+#--------------------------------------
 #create an AF_INET, STREAM socket (TCP)
-#-------------------------------------
+#--------------------------------------
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 print (" Client Socket is Created")
 
@@ -32,12 +32,53 @@ except socket.error as msg:
 	
 print (" Socket bind completed")
 
-#---------------------------
-# Start listening on socket
-#---------------------------
+#------------------------------------------------
+# Start listening on socket for client connection
+#------------------------------------------------
+# The parameter of the function listen is called backlog. It controls the number 
+# of incoming connections that are kept "waiting" if the program is already busy. 
+# So by specifying 10, it means that if 10 connections are already waiting 
+# to be processed, then the 11th connection request shall be rejected. This will 
+# be more clear after checking socket_accept.
 s.listen(10)
 print ("Socket now listening")
 
+#-------------------------------------- 
+# accept connection from client client
+#--------------------------------------
+# wait to accept a connection - blocking call: waits till client shows up
+conn, addr = s.accept()
+print ("Connected with " + addr[0] + ":" + str(addr[1]))
+
+#--------------------------------- 
+# now keep talking with the client
+#---------------------------------
+# infinite loop so that function do not terminate and thread do not end.
+while True:
+	print ("waiting to receive data from client")
+	#Receiving from client
+	data = conn.recv(1024)
+	print ("data received from client:  " + data.decode('utf-8'))
+	#print(data.decode('utf-8')) # print received data
+	
+	if (data.decode('utf-8')) == 'end':
+		print("end rcvd, closing connection")
+		break
+	
+	reply = "OK..." + data.decode('utf-8')
+	if not data: 
+		print("no data, closing connection")
+		break
+	
+	conn.sendall(reply.encode('utf-8'))
+ 
+#came out of loop
+conn.close()
+
+
+#---------------------------------------- 
+# Code for connecting to multiple client
+#----------------------------------------
 #-----------------------------
 # handle clients
 #-----------------------------
@@ -49,9 +90,10 @@ def clientthread(conn):
      
     #infinite loop so that function do not terminate and thread do not end.
 	while True:
-         
+		print ("waiting to receive data from client")
         #Receiving from client
 		data = conn.recv(1024)
+		print ("data received from client")
 		print(data.decode('utf-8')) # print received data
 		
 		if (data.decode('utf-8')) == 'end':
@@ -59,6 +101,7 @@ def clientthread(conn):
 		
 		reply = "OK..." + data.decode('utf-8')
 		if not data: 
+			print("no data, closing connection")
 			break
         
 		conn.sendall(reply.encode('utf-8'))
@@ -66,17 +109,17 @@ def clientthread(conn):
     #came out of loop
 	conn.close()
 
-
-#--------------------------------- 
-# now keep talking with the client
-#---------------------------------
-while 1:
+# This while loops waits for new client connections. If you are going to use just one client
+# then, you don't need this while loop
+##while 1:
     #wait to accept a connection - blocking call
-    conn, addr = s.accept()
-    print ("Connected with " + addr[0] + ":" + str(addr[1]))
+	
+	# Including the following 1 line of code makes the server connet to multiple clients
+    ## conn, addr = s.accept()
+    ## print ("Connected with " + addr[0] + ":" + str(addr[1]))
 	
 	#start new thread takes 1st argument as a function name to be run, second is the tuple of arguments to the function.
-    _thread.start_new_thread(clientthread ,(conn,))
+    ##_thread.start_new_thread(clientthread ,(conn,))
      
 s.close()
 
